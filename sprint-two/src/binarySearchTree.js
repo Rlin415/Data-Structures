77669bff1d4808f70867a9de9ce430dd172e88cd
Robert Lin <Rlin415@gmail.com>
2015-11-20 16:33:50 -0800

    Finished

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,44 @@
 var BinarySearchTree = function(value){
+  var obj = {
+    value: value,
+    left: null,
+    right: null
+  };
+  _.extend(obj, binaryMethods);
+  return obj;
 };
 
+var binaryMethods = {
+  insert: function(value) {
+    var node = this;
+    var current = value > node.value ? 'right' : 'left';
 
+    while(node[current]) {
+      node = node[current];
+      if (node.value > value) {
+        current = 'left';
+      } else {
+        current = 'right';
+      }
+    }
+    node[current] = BinarySearchTree(value);
+  },
+  contains: function(target) {
+    var node = this;
+    var current = target > node.value ? 'right' : 'left';
+    var result = (target === node.value);
+
+    while (node[current]) {
+      node = node[current];
+      result = (target === node.value);
+      current = target > node.value ? 'right' : 'left';
+    }
+    return result;
+  },
+  depthFirstLog: function() {
+
+  }
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */

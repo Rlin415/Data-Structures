2dfb5f50887df607e64d5a472ddee649db2895ce
Robert Lin <Rlin415@gmail.com>
2015-11-21 14:58:27 -0800

    Finished extra credit

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,28 +1,96 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._count = 0;
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage[i] = this._storage[i] || {};
-  this._storage[i][k] = v;
+  var arr = this._storage.get(i);
+  var exists = false;
+
+  if (!arr) {
+    arr = [];
+    this._storage.set(i, arr);
+  }
+
+  for (var i=0; i<arr.length; i++) {
+    if (arr[i][0] === k) {
+      arr[i][1] = v;
+      exists = true;
+      break;
+    }
+  }
+
+  if (!exists) {
+    arr.push([k,v]);
+
+    this._count++;
+
+    if (this._count > this._limit * 0.75) {
+      this.resize(this._limit * 2);
+    }
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage[i][k];
+  var output = null;
+  var temp;
+
+  temp = this._storage.get(i);
+
+  _.each(temp, function(value) {
+    if (value[0] === k) {
+      output = value[1];
+    }
+  });
+
+  return output;
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage[i][k] = null;
+  var arr = this._storage.get(i);
+
+  for (var i=0; i<arr.length; i++) {
+    if (arr[i][0] === k) {
+
+      arr.splice(k);
+
+      this._count--;
+
+      if (this._count < this._limit * 0.25) {
+        this.resize(this._limit / 2);
+      }
+      break;
+    }
+  }
+};
+
+HashTable.prototype.resize = function(newLimit) {
+
+  var currentStorage = this._storage;
+  var table = this;
+
+  this._storage = LimitedArray(newLimit);
+  this._limit = newLimit;
+  this._count = 0;
+
+  currentStorage.each(function(item) {
+    if (item) {
+      _.each(item, function(arr) {
+        table.insert(arr[0], arr[1]);
+      });
+    }
+  });
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
 
-//insert: O(n) (insert is constant, hashing algo depends but here is linear)
-//retrieve: O(n) (insert is constant, hashing algo depends but here is linear)
-//remove: O(n) (insert is constant, hashing algo depends but here is linear)
+//insert: O(n) (insert is linear, hashing algo depends but here is linear)
+//retrieve: O(n) (insert is linear, hashing algo depends but here is linear)
+//remove: O(n) (insert is linear, hashing algo depends but here is linear)
+//resize: O(n^2)
